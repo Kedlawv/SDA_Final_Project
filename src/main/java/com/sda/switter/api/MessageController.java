72cab923fe761d59d4de2895d9a52a8a4b5e2983
Ewa <malecewka@gmail.com>
2019-12-07 15:09:30 +0100

    MessageController | basic thymeleaf view "hello" in resources/templates

---
+++ src/main/java/com/sda/switter/api/MessageController.java
@@ -0,0 +1,16 @@
+package com.sda.switter.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/messages")
+public class MessageController {
+
+    @GetMapping
+    public String hello(){
+        return "hello";
+    }
+}
